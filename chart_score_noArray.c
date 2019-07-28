45a2831828d6e4db7a32213bcae7f9e2c203e92e
clayton <clayton.hutton@gmail.com>
2019-07-28 12:16:56 -0700

    Finished coding of file to implement for one score, no arrays or global vars

--- chart_score_noArray.c
+++ chart_score_noArray.c
@@ -1,3 +1,5 @@
+
+// This program will chart a score and print as hashes, one per point.
 #include <cs50.h>
 #include <stdio.h>
 
@@ -5,5 +7,19 @@ void chart(int score);
 
 int main(void)
 {
-	int score1 = get_int("Enter Score 1")
+	int score1 = get_int("Enter Score 1");
+
+	for (int i = 0 ; i < 1; i++)
+	{
+		chart(score1);
+	}
+}
+
+void cart(int score);
+{
+	for (int i = 0; i < score; i++)
+	{
+		printf("#");
+	}
+	printf("\n");
 }
